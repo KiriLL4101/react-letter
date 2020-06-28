1c92b64b4b587931b0220c58b1c01359156483f6
kiril <fedechkin-kiril@mail.ru>
2020-06-28 20:25:56 +0300

    Added changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React from 'react';
 import { Header, BookCard, Filter} from './components'
 import { Container, Card } from 'semantic-ui-react'
+import axios from 'axios';
 
 import './app.scss'
-import books from './db.json'
 
 
 function App() {
   const [ activeItem, setActiveItem] = React.useState("all");
-
+  const [ books , setBooks ] = React.useState(null)
+  React.useEffect( () => {
+    axios.get('http://localhost:3001/books').then(({ data }) => {
+      setBooks(data)
+    });
+  }, [])
+  function onClickActiveItem(name){
+    setActiveItem(name)
+  }
   return (
     <div className="App">
       <Container>
         <Header/>
-        <Filter activeItem={activeItem} onClickItem={name => setActiveItem(name)}/>
-        <Card.Group itemsPerRow={5}>
+        <Filter activeItem={activeItem} onClickItem={onClickActiveItem}/>
+        <Card.Group itemsPerRow={4}>
             {
-              books.books &&
-               books.books.map( book => (
+              books &&
+               books.map( book => (
                 <BookCard key={book.id} {...book}/>
                ))
             }
