abfb9f56f993ba045c61a53350e61aa84540eda7
kiril <fedechkin-kiril@mail.ru>
2020-07-14 23:20:13 +0300

    add redux books

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import React from 'react';
-import { Header, BookCard, Filter} from './components'
-import { Container, Card } from 'semantic-ui-react'
-import axios from 'axios';
+import React from "react";
+import { Header, BookCard, Filter } from "./components";
+import { Container, Card } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { setBooks } from "./redux/action/books";
+import axios from "axios";
 
-import './app.scss'
+import "./app.scss";
 
-
-function App() {
-  const [ activeItem, setActiveItem] = React.useState("all");
-  const [ books , setBooks ] = React.useState(null)
-  React.useEffect( () => {
-    axios.get('http://localhost:3001/books').then(({ data }) => {
-      setBooks(data)
+function App(props) {
+  const [activeItem, setActiveItem] = React.useState("all");
+  // const [books, setBooks] = React.useState(null);
+  React.useEffect(() => {
+    axios.get("http://localhost:3001/books").then(({ data }) => {
+      console.log(data);
+      props.setBooks(data);
     });
-  }, [])
-  function onClickActiveItem(name){
-    setActiveItem(name)
+  }, []);
+  function onClickActiveItem(name) {
+    setActiveItem(name);
   }
+
+  console.log(props);
   return (
     <div className="App">
       <Container>
-        <Header/>
-        <Filter activeItem={activeItem} onClickItem={onClickActiveItem}/>
+        <Header />
+        <Filter activeItem={activeItem} onClickItem={onClickActiveItem} />
         <Card.Group itemsPerRow={4}>
-            {
-              books &&
-               books.map( book => (
-                <BookCard key={book.id} {...book}/>
-               ))
-            }
+          {!props.books
+            ? "Загрузка..."
+            : props.books.map((book) => <BookCard key={book.id} {...book} />)}
         </Card.Group>
       </Container>
     </div>
   );
 }
+const mapStateToProps = (state) => {
+  console.log(state)
+  return {
+    books: state.items,
+  };
+};
 
-export default App;
+const mapDipatchToProps = {
+  setBooks
+};
+export default connect(mapStateToProps, mapDipatchToProps)(App);
